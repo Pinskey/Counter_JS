d290455c1cb9bdd6f748d663fdaa02caec883205
Pinskey <francescopisaneschi.lavoro@gmail.com>
2025-11-20 17:29:18 +0100

    sesto commit

--- counter.js
+++ counter.js
@@ -1,28 +1,48 @@
-/*  BUTTON '-'  */
+/* DIV Sottrai */
+/* Si crea il div che conterrà titolo e pulsante per la sottrazione */
 const divSub = document.createElement('div');
+/* Si assegna una classe a tale div */
 divSub.className = 'd_sub';
+
+/* TITOLO 'Sottrai 1' */
+/* Si crea un titolo per identificare lo scopo del pulsante */
 const titleSub = document.createElement('h2');
-titleSub.className = 'titleSub';
+/* Si assegna una classe e il testo al titolo */
+titleSub.className = 'title_sub';
 titleSub.textContent = 'Sottrai 1';
+/* Si aggiunge il Titolo al div */
 divSub.appendChild(titleSub);
-/* Si crea il pulsante per togliere al contatore */
+/* Fine TITOLO 'Sottrai 1' */
+
+/*  BUTTON '-'  */
+/* Si crea il pulsante per sottrarre al contatore */
 const btnSub = document.createElement('button');
 /* Si crea un div dentro il pulsante per gestire meglio il testo */
 const divMinus = document.createElement('div');
-/* Si va ad assegnare una classe al pulsante e al div inoltre si assegna simbolo '-' al div */
+/* Si va ad assegnare una classe al pulsante e al div */
 btnSub.className = 'btn sub';
 divMinus.className = 'd_minus';
+/* Si assegna il simbolo '-' al div come testo */
 divMinus.textContent = '-';
 /* Si aggiungono i vari elementi creati alla pagina */
 divSub.appendChild(btnSub);
 btnSub.appendChild(divMinus);
+/* Fine BUTTON '-' */
+
+/* Si aggiunge il div sottrai alla pagina */
 document.body.append(divSub);
+
+/* Fine DIV Sottrai */
 
 
 /* CONTATORE */
+/* Si crea il div che conterrà titolo e pulsante per aggiungere */
 const divCounter = document.createElement('div');
+/* Si assegna una classe a tale div */
 divCounter.className = 'd_counter';
+/* Si crea titolo per identificare il Contatore  */
 const titleCounter = document.createElement('h1');
+/* Si assegna una classe ed un testo al titolo */
 titleCounter.className = 't_counter';
 titleCounter.textContent = 'Contatore';
 /* Si crea il counter inizializzato a 0 */
@@ -36,25 +56,41 @@ divCounter.appendChild(titleCounter);
 divCounter.appendChild(counter);
 
 
-/*  BUTTON '+'  */
+/* DIV Aggiungi */
+/* Si crea il div che conterrà titolo e pulsante per aggiungere */
 const divAdd = document.createElement('div');
+/* Si assegna una classe a tale div */
 divAdd.className = 'd_add';
+
+/* TITOLO 'Aggiungi 1' */
+/* Si crea un titolo per identificare lo scopo del pulsante */
 const titleAdd = document.createElement('h2');
-titleAdd.className = 'titleAdd';
+/* Si assegna una classe e il testo al titolo */
+titleAdd.className = 'title_add';
 titleAdd.textContent = 'Aggiungi 1';
+/* Si aggiunge il Titolo al div */
 divAdd.appendChild(titleAdd);
+/* Fine TITOLO 'Aggiungi 1' */
+
+/*  BUTTON '+'  */
 /* Si crea il pulsante per aggiungere al contatore */
 const btnAdd = document.createElement('button');
 /* Si crea un div dentro il pulsante per gestire meglio il testo */
 const divPlus = document.createElement('div');
-/* Si va ad assegnare una classe al pulsante e al div inoltre si assegna simbolo '+' al div */
+/* Si va ad assegnare una classe al pulsante e al div */
 btnAdd.className = 'btn add';
 divPlus.className = 'd_plus'
+/* Si assegna simbolo '+' al div come testo */
 divPlus.textContent = '+';
 /* Si aggiungono i vari elementi creati alla pagina */
 divAdd.appendChild(btnAdd);
 btnAdd.appendChild(divPlus);
+/* Fine BUTTON '+' */
+
+/* Si aggiunge il div aggiungi alla pagina */
 document.body.appendChild(divAdd);
+
+/* Fine DIV Aggiungi */
 
 
 /* IMPLEMENTAZIONE FUNZIONI CONTATORE */
@@ -74,3 +110,4 @@ btnSub.addEventListener('click', ()=>{
         alert('Non sono permessi numeri negativi!');
     }
 })
+
