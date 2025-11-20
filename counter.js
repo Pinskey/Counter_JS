9dca9a715f5ea895cc295fb4977de2c4114dc6c0
Pinskey <francescopisaneschi.lavoro@gmail.com>
2025-11-20 11:25:13 +0100

    quarto commit

--- counter.js
+++ counter.js
@@ -1,3 +1,27 @@
+/*  BUTTON '-'  */
+/* Si crea il pulsante per togliere al contatore */
+const btnSub = document.createElement('button');
+/* Si crea un div dentro il pulsante per gestire meglio il testo */
+const divMinus = document.createElement('div');
+/* Si va ad assegnare una classe al pulsante e al div inoltre si assegna simbolo '-' al div */
+btnSub.className = 'btn sub';
+divMinus.className = 'div minus';
+divMinus.textContent = '-';
+/* Si aggiungono i vari elementi creati alla pagina */
+document.body.appendChild(btnSub);
+btnSub.appendChild(divMinus);
+
+
+/* CONTATORE */
+/* Si crea il counter inizializzato a 0 */
+const counter = document.createElement('div');
+/* Si va ad assegnare una classe al div e si assegna un testo di default */
+counter.className = 'counter';
+counter.textContent = '0'
+/* Si aggiungono i vari elementi creati alla pagina */
+document.body.appendChild(counter);
+
+
 /*  BUTTON '+'  */
 /* Si crea il pulsante per aggiungere al contatore */
 const btnAdd = document.createElement('button');
@@ -11,27 +35,6 @@ divPlus.textContent = '+';
 document.body.appendChild(btnAdd);
 btnAdd.appendChild(divPlus);
 
-/* CONTATORE */
-/* Si crea il counter inizializzato a 0 */
-const counter = document.createElement('div');
-/* Si va ad assegnare una classe al div e si assegna un testo di default */
-counter.className = 'counter';
-counter.textContent = '0'
-/* Si aggiungono i vari elementi creati alla pagina */
-document.body.appendChild(counter);
-
-/*  BUTTON '-'  */
-/* Si crea il pulsante per togliere al contatore */
-const btnSub = document.createElement('button');
-/* Si crea un div dentro il pulsante per gestire meglio il testo */
-const divMinus = document.createElement('div');
-/* Si va ad assegnare una classe al pulsante e al div inoltre si assegna simbolo '-' al div */
-btnSub.className = 'btn sub';
-divMinus.className = 'div minus';
-divMinus.textContent = '-';
-/* Si aggiungono i vari elementi creati alla pagina */
-document.body.appendChild(btnSub);
-btnSub.appendChild(divMinus);
 
 /* IMPLEMENTAZIONE FUNZIONI CONTATORE */
 /* Si converte il valore del counter da numero a stringa per potervi sommare e sottrarre ad ogni click*/
